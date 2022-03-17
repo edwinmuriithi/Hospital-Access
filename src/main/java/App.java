bad9bab7a959919ecf8455730d72cd126477886d
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-17 12:21:36 +0300

    Connect local db

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,20 @@
+import com.google.gson.Gson;
+import models.dao.Sql2oTreatmentDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class App {
+    public static void main (String[] args){
+        Gson gson= new Gson();
+        Sql2oTreatmentDao treatmentDao;
+
+
+        //Connection to local db
+        String connectionString = "jdbc:postgresql://localhost:5432/hospitalaccess";
+        Sql2o sql2o = new Sql2o(connectionString, "muriithi", "123456");
+
+        treatmentDao= new Sql2oTreatmentDao(sql2o);
+        Connection conn;
+        conn = sql2o.open();
+    }
 }
